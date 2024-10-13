5581cf2ca071c1b10a777ef19c2f91a018e64788
Mikhailpuv <mikhailpuv@gmail.com>
2024-10-13 19:19:24 +0300

    Update import 1pygame.py

--- import 1pygame.py
+++ import 1pygame.py
@@ -14,15 +14,13 @@ white = (255, 255, 255)
 pink = (255, 192, 203)
 dark_green = (0, 100, 0)
 
-# Функция для рисования дома
 def draw_house(x, y):
-    pygame.draw.rect(screen, brown, (x, y, 120, 80))  # Дом
-    pygame.draw.rect(screen, blue, (x + 40, y + 20, 40, 40))  # Окно
-    pygame.draw.polygon(screen, red, [(x, y), (x + 60, y - 40), (x + 120, y)])  # Крыша
+    pygame.draw.rect(screen, brown, (x, y, 120, 80))  
+    pygame.draw.rect(screen, blue, (x + 40, y + 20, 40, 40)) 
+    pygame.draw.polygon(screen, red, [(x, y), (x + 60, y - 40), (x + 120, y)])  
 
-# Функция для рисования дерева
 def draw_tree(x, y):
-    pygame.draw.rect(screen, brown, (x + 10, y, 10, 60))  # Ствол
+    pygame.draw.rect(screen, brown, (x + 10, y, 10, 60)) 
     pygame.draw.circle(screen, dark_green, (x + 5, y), 15)
     pygame.draw.circle(screen, dark_green, (x + 30, y), 15)
     pygame.draw.circle(screen, dark_green, (x + 15, y - 15), 15)
@@ -31,7 +29,6 @@ def draw_tree(x, y):
     pygame.draw.circle(screen, dark_green, (x + 17, y - 30), 15)
 
 
-# Основной цикл
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,20 +36,16 @@ while True:
             sys.exit()
     pygame.display.flip()
 
-    # Заполнение фона
     screen.fill(green)
-    pygame.draw.rect(screen, blue, (0, 0, 600, 400 // 2))  # Голубая половина
+    pygame.draw.rect(screen, blue, (0, 0, 600, 400 // 2))  
 
-    # Рисуем дома
+
     draw_house(100, 250)
-
     draw_house(350, 230)
 
-    # Рисуем деревья
     draw_tree(250, 230)
     draw_tree(500, 250)
 
-    # Рисуем облака
     def draw_cloud(x, y):
         pygame.draw.circle(screen, white, (x + 10, y), 30)
         pygame.draw.circle(screen, white, (x + 30, y), 30)
@@ -65,9 +58,6 @@ while True:
     draw_cloud(350, 100)
     draw_cloud(500, 60)
 
-
-
-    # Рисуем солнце
     pygame.draw.circle(screen, pink, (50, 50), 30)
 
 
